44d44051a7a27cf9a82cf9d55058c236412ef8fc
hlaingtinhtun <hlaingtinhtun@gmail.com>
2020-03-07 18:26:26 +0900

    http client

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $data = Http::post('https://jsonplaceholder.typicode.com/posts',[
+        'title'=> 'foo',
+      'body'=> 'bar',
+      'userId'=> 1
+    ]);
+
+    dd($data->body());
 });
